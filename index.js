a72973371e9e5e3952d24b02ced30e060f82d1a8
Ilya <tkach.io123@gmail.com>
2020-04-26 19:11:21 +0300

    Solve problem with magic numbers, changed names of variables and classes

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const MILLISECONDS_IN_SECOND = 1000;
+const SECONDS_IN_MINUTE = 60;
+const MINUTES_IN_HOURS = 60;
+const HOURS_AT_DAY = 24;
+
 class Timer {
     constructor(hours, minutes, sec, UTC) {
         this.hours = hours;
@@ -5,27 +10,27 @@ class Timer {
         this.sec = sec;
         this.UTC = UTC;
 
-        this.curTime();
+        this.currentTime();
     }
 
-    curTime = () => {
-        this.currentTime();
-        setInterval(this.currentTime, 1000);
+    currentTime = () => {
+        this.changeTime();
+        setInterval(this.changeTime, 1000);
     };
 
-    currentTime = () => {
-            const currentHours = Math.floor((Date.now() / 1000 / 60 / 60 % 24));
-            const currentMinutes = Math.floor((Date.now() / 1000 / 60 % 60));
-            const currentSec = Math.floor(Date.now() / 1000 % 60);
+    changeTime = () => {
+            const current_hours = Math.floor((Date.now() / MILLISECONDS_IN_SECOND / SECONDS_IN_MINUTE / MINUTES_IN_HOURS % HOURS_AT_DAY));
+            const current_minutes = Math.floor((Date.now() / MILLISECONDS_IN_SECOND / SECONDS_IN_MINUTE % MINUTES_IN_HOURS));
+            const current_seconds = Math.floor(Date.now() / MILLISECONDS_IN_SECOND % SECONDS_IN_MINUTE);
 
-            this.hours.value = currentHours <= 9 ? 0 + (+currentHours + this.UTC).toString() : +currentHours + this.UTC;
-            this.minutes.value = currentMinutes <= 9 ? 0 + currentMinutes.toString() : currentMinutes;
-            this.sec.value = currentSec <= 8 ? 0 + currentSec.toString() : currentSec;
+            this.hours.value = current_hours <= 9 ? 0 + (+current_hours + this.UTC).toString() : +current_hours + this.UTC;
+            this.minutes.value = current_minutes <= 9 ? 0 + current_minutes.toString() : current_minutes;
+            this.sec.value = current_seconds <= 9 ? 0 + current_seconds.toString() : current_seconds;
     }
 }
 
-const hours = document.querySelector('#inp1');
-const minutes = document.querySelector('#inp2');
-const sec = document.querySelector('#inp3');
+const hours = document.querySelector('#hours');
+const minutes = document.querySelector('#minutes');
+const sec = document.querySelector('#seconds');
 
 const time = new Timer(hours, minutes, sec, 3);
